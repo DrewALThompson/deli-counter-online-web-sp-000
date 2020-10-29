2cfb235a5e4273835fb4df41b2ef1a7b14dcd292
Drew Thompson <drewallynthompson@gmail.com>
2020-10-29 23:35:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,13 +2,15 @@ katz_deli = []
 
 def line(katz_deli)
   deli_line = ""
- if katz_deli.length > 0 
-   katz_deli.each.with_index(1){|name, number| deli_line << " #{number}. #{name}" }
-   puts "The line is currently:#{deli_line}"
- else 
-   puts "The line is currently empty."
+if katz_deli.length > 0 
+  katz_deli.each.with_index(1){|name, number| deli_line << " #{number}. #{name}" }
+  puts "The line is currently:#{deli_line}"
+else 
+  puts "The line is currently empty."
 end
 end
+
+  
 
 def take_a_number(katz_deli, next_person)
   katz_deli << next_person
@@ -17,9 +19,9 @@ end
 
 def now_serving(katz_deli)
   if katz_deli.length > 0
-  puts "Currently serving #{katz_deli[0]}."
-  katz_deli.shift
-else
-  puts "There is nobody waiting to be served!"
-end
+   puts "Currently serving #{katz_deli[0]}."
+   katz_deli.shift
+  else
+   puts "There is nobody waiting to be served!"
+ end
 end
